c362da477edca16651597f2a2c215b3764d1e2ab
adamdev <adamdevofficial@protonmail.com>
2025-04-11 07:34:57 -0400

    Changed default HUD settings

--- src/Core/Settings.cs
+++ src/Core/Settings.cs
@@ -2,8 +2,8 @@
 {
     public static class Settings
     {
-        public static float DistanceToCamera = 1.25f;
-        public static float MovementSmoothness = 8f;
+        public static float DistanceToCamera = 1.5f;
+        public static float MovementSmoothness = 16f;
 
         public static bool UseAnnouncer = true;
     }
