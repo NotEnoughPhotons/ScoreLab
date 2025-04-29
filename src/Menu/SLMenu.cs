279e6e16e06ca4e698e546d2c70ef53b40d1dd89
adamdev <adamdevofficial@protonmail.com>
2025-04-29 11:21:37 -0400

    Renamed Menu to SLMenu

---
+++ src/Menu/SLMenu.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using BoneLib.BoneMenu;
+using NEP.ScoreLab.Core;
+using NEP.ScoreLab.Data;
+using NEP.ScoreLab.HUD;
+
+namespace NEP.ScoreLab.Menu
+{
+    public static class SLMenu
+    {
+        private static Page _hudPage;
+        
+        public static void Initialize()
+        {
+            Page root = Page.Root.CreatePage("Not Enough Photons", Color.white);
+            Page modPage = root.CreatePage("ScoreLab", Color.white);
+            _hudPage = modPage.CreatePage("HUDs", Color.white);
+
+            modPage.CreateFunction("Reload HUDs", Color.white, () => HUDLoader.ReloadHUDs());
+            
+            for (int i = 0; i < HUDLoader.LoadedHUDManifests.Count; i++)
+            {
+                int index = i;
+                var manifest = HUDLoader.LoadedHUDManifests[index];
+
+                var function = _hudPage.CreateFunction(manifest.Name, Color.white, () => HUDManager.Instance.LoadHUD(manifest.Name));
+                function.Logo = manifest.Logo;
+            }
+        }
+
+        public static void RefreshHUDPage()
+        {
+            _hudPage.RemoveAll();
+            
+            for (int i = 0; i < HUDLoader.LoadedHUDManifests.Count; i++)
+            {
+                int index = i;
+                var manifest = HUDLoader.LoadedHUDManifests[index];
+
+                var function = _hudPage.CreateFunction(manifest.Name, Color.white, () => HUDManager.Instance.LoadHUD(manifest.Name));
+                function.Logo = manifest.Logo;
+            }
+        }
+    }
+}
