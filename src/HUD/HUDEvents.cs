ae7d357ded9a7999c0a72c2bbbfd555d648bc902
adamdev <adamdevofficial@protonmail.com>
2025-06-16 15:17:11 -0400

    UltEvent type checking

--- src/HUD/HUDEvents.cs
+++ src/HUD/HUDEvents.cs
@@ -3,6 +3,7 @@ using Il2CppInterop.Runtime.InteropTypes.Fields;
 using Il2CppUltEvents;
 using MelonLoader;
 using NEP.ScoreLab.Core;
+using NEP.ScoreLab.Data;
 using UnityEngine;
 
 namespace NEP.ScoreLab.HUD
@@ -17,23 +18,35 @@ namespace NEP.ScoreLab.HUD
 
         private void Awake()
         {
-            API.Value.OnValueAdded += (value) => OnScoreUpdateEvent_Callback();
+            API.Value.OnValueAdded += OnScoreUpdateEvent_Callback;
+            API.Value.OnValueAdded += OnMultiplierUpdateEvent_Callback;
         }
 
         private void OnDestroy()
         {
-            API.Value.OnValueAdded -= (value) => OnScoreUpdateEvent_Callback();
+            API.Value.OnValueAdded -= OnScoreUpdateEvent_Callback;
+            API.Value.OnValueAdded -= OnMultiplierUpdateEvent_Callback;
         }
 
         [HideFromIl2Cpp]
-        private void OnScoreUpdateEvent_Callback()
+        private void OnScoreUpdateEvent_Callback(PackedValue value)
         {
+            if (value is not PackedScore)
+            {
+                return;
+            }
+            
             OnScoreUpdateEvent.Get()?.Invoke();
         }
 
         [HideFromIl2Cpp]
-        private void OnMultiplierUpdateEvent_Callback()
+        private void OnMultiplierUpdateEvent_Callback(PackedValue value)
         {
+            if (value is not PackedMultiplier)
+            {
+                return;
+            }
+            
             OnMultiplierUpdateEvent.Get()?.Invoke();
         }
     }
